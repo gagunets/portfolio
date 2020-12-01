a03155b995a36b7edaa512a8649ebfd5f6d99f8e
Danylo Sokolov <gagunets@gmail.com>
2020-12-01 02:58:13 +0200

    blog-page

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+
+
 // On Click hand emoji will take you to the top of the page
 document.getElementById('top-button').addEventListener('click',function(){
     window.scrollTo(0,0);
@@ -20,24 +22,28 @@ $(window).scroll(function(){
 // Toggle between Day and Night mode
 $('#switch1').on('click', function(){
   $('body').toggleClass('night')
-})
+});
+
+
 
 
 // If user visit after 7 pm in night the body will change the style to night class
-$(document).ready(function(){
-  var date = new Date();
-  var current_time = date.getHours();
-  if (current_time > 19 || current_time < 6)
-    // If time is after 7PM or before 6AM, apply night theme to ‘body’
-    document.body.className = "night";
-  else
-    // Else use ‘day’ theme
-    document.body.className = "";
-});
+// $(document).ready(function(){
+//   var date = new Date();
+//   var current_time = date.getHours();
+//   if (current_time > 19 || current_time < 6)
+//     // If time is after 7PM or before 6AM, apply night theme to ‘body’
+//     document.body.className = "night";
+//   else
+//     // Else use ‘day’ theme
+//     document.body.className = "";
+// });
+//
+// $(window).load(function() {
+//       $("#top-button").hide();
+// });
 
-$(window).load(function() {
-      $("#top-button").hide();
-});
+
 
 
 // If you hover over the languages I used in project Box it will do fade in animation
@@ -53,3 +59,39 @@ $('.project_used span').on({
 });
 
 
+
+// Header Logo
+
+
+$(function() { var logo = $(".logo-icon-none"); $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+        if(!logo.hasClass("logo-icon")) {
+            logo.hide();
+            logo.removeClass('logo-icon-none').addClass("logo-icon").fadeIn( "slow");
+        }
+    } else {
+        if(!logo.hasClass("logo-icon-none")) {
+            logo.hide();
+            logo.removeClass("logo-icon").addClass('logo-icon-none').fadeIn( "slow");
+        }
+    }
+
+});
+});
+
+
+
+
+
+// Header Shadow
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+        $("#nav-header-shadow").addClass("shadow-active");
+    }
+    else {
+        $("#nav-header-shadow").removeClass("shadow-active");
+    }
+});
